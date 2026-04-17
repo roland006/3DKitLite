4bd29faaa203cd78f547944ff2ac8eb0a65c5dd3
Katia <katkanze@gmail.com>
2026-04-17 23:00:01 +0300

    Enemies+Coins

---
+++ Assets/3DGamekitLite/Scripts/CoinPickup.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+   
+    public TextMeshProUGUI uiText;
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log(other);
+        if (other.CompareTag("Player"))
+        {
+            int current = int.Parse(uiText.text);
+            uiText.text = (current + 1).ToString();
+          
+            Destroy(gameObject);
+        }
+    }
+
+}
