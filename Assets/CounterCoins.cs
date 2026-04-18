565710c3977f320fb21c22e8a396796a3f62785f
Student <Student@BHSAD.NET>
2026-04-18 17:20:00 +0300

    Coin

---
+++ Assets/CounterCoins.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Gamekit3D.Message;
+using UnityEngine.Serialization;
+
+public class CounterCoins : MonoBehaviour
+{
+    public int Coins;
+}
